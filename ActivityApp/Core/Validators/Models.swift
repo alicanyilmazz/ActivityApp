cd16668edcd94085ff99e9c3d0b736fe0ab543bd
alicanyilmazz <alicanyilmaz101@gmail.com>
2022-03-06 22:11:50 +0300

    Models added.

---
+++ ActivityApp/Core/Validators/Models.swift
@@ -0,0 +1,21 @@
+//
+//  Models.swift
+//  ActivityApp
+//
+//  Created by alican on 6.03.2022.
+//
+
+import Foundation
+
+final class UITextFieldComponent : UIComponent{
+    override init(componentType: ComponentType, validations: [ValidatorManager] = []) {
+        super.init(componentType: componentType, validations: validations)
+    }
+}
+
+final class UIDateComponent : UIComponent{
+    override init(componentType: ComponentType, validations: [ValidatorManager] = []) {
+        super.init(componentType: componentType, validations: validations)
+    }
+}
+
