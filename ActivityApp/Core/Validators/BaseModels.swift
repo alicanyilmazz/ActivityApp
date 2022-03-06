a01d5c008e63b31655721c5cbae2c15b376a7cec
alicanyilmazz <alicanyilmaz101@gmail.com>
2022-03-06 22:10:40 +0300

    BaseModels added.

---
+++ ActivityApp/Core/Validators/BaseModels.swift
@@ -0,0 +1,46 @@
+//
+//  BaseModels.swift
+//  ActivityApp
+//
+//  Created by alican on 6.03.2022.
+//
+
+import Foundation
+
+class UIComponent : UIComponentProtocol,Hashable{
+    let id: UUID = UUID()
+    let componentType: ComponentType
+    var value: Any?
+    var validations: [ValidatorManager]
+    
+    func hash(into hasher: inout Hasher) {
+            hasher.combine(id)
+    }
+    
+    static func == (lhs: UIComponent, rhs: UIComponent) -> Bool{
+            lhs.id == rhs.id
+    }
+    
+    init(componentType : ComponentType , validations: [ValidatorManager] = []){
+        self.componentType = componentType
+        self.validations = validations
+    }
+}
+
+class UISectionComponent : UISectionComponentProtocol,Hashable{
+    let id: UUID = UUID()
+    var components: [UIComponent]
+    
+    func hash(into hasher: inout Hasher) {
+            hasher.combine(id)
+    }
+    
+    static func == (lhs: UISectionComponent, rhs: UISectionComponent) -> Bool{
+            lhs.id == rhs.id
+        }
+    
+    required init(components: [UIComponent] = []){
+        self.components = components
+    }
+}
+
