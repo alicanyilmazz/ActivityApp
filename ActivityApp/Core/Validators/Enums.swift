de03a2b429c717e70ae4febbba18e1b81b84faa7
alicanyilmazz <alicanyilmaz101@gmail.com>
2022-03-11 21:21:16 +0300

    ComponentType sections refactored.

--- ActivityApp/Core/Validators/Enums.swift
+++ ActivityApp/Core/Validators/Enums.swift
@@ -7,11 +7,6 @@
 
 import Foundation
 
-enum SectionName : Int{
-    case activity = 0
-    case signin = 1
-    case signup = 2
-}
 
 enum CustomValidationError : Error{
     case custom(message : String)
@@ -20,15 +15,37 @@ enum CustomValidationError : Error{
 enum RegexValidationPatterns{
     static let email = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{3,64}"
     static let between3to20 = "^.{3,20}$"
-    static let alphabetic = "^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$"
+    static let alphabetic = "[a-zA-Z]"
     static let between3to15 = "^.{3,15}$"
+    static let onlyNumber = "^[0-9]+$"
+    static let onlyDecimalNumber = "^-?[0-9][0-9,\\.]+$"
+    static let between1to6 = "^.{1,6}$"
 }
 
-enum ComponentType: Int{
-    case activityName = 0
-    case name = 1
-    case surname = 2
-    case email = 3
+enum SectionName{
+    static let activity = 0
+    static let payment = 1
+    static let edit = 2
+}
+
+enum ComponentType {
+    
+    enum activitySection{
+        static let activityName = 0
+    }
+    
+    enum paymentSection{
+        static let payerName = 0
+        static let explanation = 1
+        static let cost = 2
+    }
+    
+    enum editSection{
+        static let payerName = 0
+        static let explanation = 1
+        static let cost = 2
+    }
+    
 }
 
 struct RegexModel{
