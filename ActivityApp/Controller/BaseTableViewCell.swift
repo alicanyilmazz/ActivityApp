4557bc700ac29f7675664bf8c7cf614a21069b9f
alicanyilmazz <alicanyilmaz101@gmail.com>
2022-03-12 23:03:26 +0300

    BaseTableViewCell added.

---
+++ ActivityApp/Controller/BaseTableViewCell.swift
@@ -0,0 +1,41 @@
+//
+//  BaseTableViewCell.swift
+//  ActivityApp
+//
+//  Created by alican on 12.03.2022.
+//
+
+import UIKit
+
+class BaseTableViewCell: UITableViewCell {
+
+    
+    @IBOutlet weak var cellLeftIcon: UIImageView!
+    @IBOutlet weak var cellLbl: UILabel!
+    @IBOutlet weak var cellLeftLbl: UILabel!
+    @IBOutlet weak var cellRightIcon: UIImageView!
+
+    @IBOutlet weak var eachView: UIView!
+    @IBOutlet weak var outsideView: UIView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        setUIFeatures()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+    func setAttributes(activityName : String , price : String){
+        cellLbl.text = activityName
+        cellLeftLbl.text = price
+    }
+    
+    func setUIFeatures(){
+       
+    }
+}
